7d023866f345fac34a75bd9e04c571f3fe5b03c4
JustVic52 <pimpo905@gmail.com>
2026-03-09 14:30:59 +0100

    primer push de backend

---
+++ backend/src/test/java/com/openpaw/votify/VotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpaw.votify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
