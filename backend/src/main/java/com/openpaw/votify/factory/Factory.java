965f0ddbfbd87de7ca62bed5921d52c259c031c1
Maxim <74974283+maximka76667@users.noreply.github.com>
2026-03-10 18:33:22 +0100

    refact: improve factory

--- backend/src/main/java/com/openpaw/votify/factory/Factory.java
+++ backend/src/main/java/com/openpaw/votify/factory/Factory.java
@@ -5,18 +5,19 @@ import java.sql.SQLException;
 import java.util.function.Supplier;
 
 public class Factory<T extends Creatable<T, P>, P> implements Creatable<T, P> {
-    private final Supplier<T> supplier;
+    // An instance created once when we create a factory and reused for all subsequent creations
+    private final T instance;
 
     public Factory(Supplier<T> supplier) {
-        this.supplier = supplier;
+        this.instance = supplier.get();
     }
 
     public T fromResultSet(ResultSet rs, int rowNum) throws SQLException {
-        return supplier.get().fromResultSet(rs, rowNum);
+        return instance.fromResultSet(rs, rowNum);
     }
 
     @Override
     public T fromParams(P params) {
-        return supplier.get().fromParams(params);
+        return instance.fromParams(params);
     }
 }
